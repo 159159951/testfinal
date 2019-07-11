e43e1573b11effd4ae487b99e7b4591a3e74554d
Truong Tran Quang Huy <truongtranquanghuy@gmail.com>
2019-07-12 00:20:32 +0700

    add all screen

--- app/src/main/java/com/appman/monocle/MainActivity.kt
+++ app/src/main/java/com/appman/monocle/MainActivity.kt
@@ -1,23 +1,97 @@
 package com.appman.monocle
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.agm.monocle.model.AGMMonocleConfiguration
+import com.agm.monocle.model.CopyrightWatermark
+import com.agm.monocle.model.StampWatermark
+import com.agm.monocle.opencv.CameraWithAutoCaptureActivity
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import monocle.KotlinClass
 
 class MainActivity : AppCompatActivity() {
-
+    private val OPEN_CAMERA_REQ: Int = 100
+    private lateinit var userConfigObject: AGMMonocleConfiguration;
+    private lateinit var copyrightWatermark: CopyrightWatermark;
+    private lateinit var stampWatermark: StampWatermark;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+
+        userConfigObject = AGMMonocleConfiguration(
+            null,
+            false,
+            null,
+            false,
+            -1
+        )
+
+        copyrightWatermark = CopyrightWatermark(
+            "#e9d5e7",
+            20f,
+            30f,
+            "#copyrightWatermark",
+            "#eaff4f"
+        )
+
+        stampWatermark = StampWatermark(
+            "#c4546f",
+            "#050505",
+            10f,
+            "#stampWatermark",
+            "#5530eb"
+
+        )
+
+        setupCameraView(0, true)
+        setupCameraView(1, true)
+        setupCameraView(2, true)
+        setupCameraView(3, true)
+        setupCameraView(4, true)
+
         fab.setOnClickListener { view ->
             Toast.makeText(this, KotlinClass().getStringRes(), Toast.LENGTH_SHORT).show()
+            openCamera()
+        }
+    }
 
+    private fun openCamera() {
+        var cameraIntent = Intent(this, CameraWithAutoCaptureActivity::class.java);
+        cameraIntent.putExtra("BUNDLE_USER_CONFIG", Gson().toJson(userConfigObject));
+        startActivityForResult(cameraIntent, OPEN_CAMERA_REQ)
+    }
+
+
+    private fun setupCameraView(case: Int, isChecked: Boolean) {
+        when (case) {
+            //Water mark logo
+            0 -> if (isChecked) {
+                userConfigObject.waterMarkLogo = R.drawable.water_mark_icon
+            } else {
+                userConfigObject.waterMarkLogo = -1
+            }
+            //Policy Text
+            1 -> if (isChecked) {
+                userConfigObject.copyrightWatermark = copyrightWatermark
+            } else {
+                userConfigObject.copyrightWatermark = null
+            }
+            //Stamp Text
+            2 -> if (isChecked) {
+                userConfigObject.stampWatermark = stampWatermark
+            } else {
+                userConfigObject.stampWatermark = null
+            }
+            //Toggle Crop
+            3 -> userConfigObject.isCropEnabled = isChecked
+            4 -> userConfigObject.useDetection = isChecked
         }
     }
 
